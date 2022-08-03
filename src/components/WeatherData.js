b1dc745d007defd3f5bcd08b8613ff56c7537980
jclevecodes <joshua.cleveland02@gmail.com>
2022-08-03 16:31:59 -0400

    daily commit + weather ui update

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import './WeatherDataStyling.css';
+import { API_KEY } from '../apidata/ApiKey';
 
 const WeatherData = () => {
   return (
-    <div className='weather-wrapper'>
-        <div className='weather-location'>
-            <h3>Weather in Tokyo</h3>
-        </div>
+    <div className='weather-app'>
         <div>
-            <h1 className='weather-degrees'>12.84</h1>
-        </div>
+            <div className='weather-location'>
+                Tokyo
+            </div>
+            <div className='weather-degrees'>12.84°</div>
 
-        <div className='weather-description'>
+            <div className='weather-description'>
+                Partly Cloudy
+            </div>            
+        </div>
+        
+        <div>
             <div className='weather-description-head'>
-                <div>
-                    <span className=''>⛅️</span>
-                    <h3>Partly Cloudy</h3>
-                </div>
+                <span className=''>⛅️</span>
             </div>
         </div>
+        
     </div>
     
     
