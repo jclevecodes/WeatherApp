b4fba9a91a3d4faa26ccd31e6eff16d57e98c284
jclevecodes <joshua.cleveland02@gmail.com>
2022-08-03 16:51:11 -0400

    data fetcher setup

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './WeatherDataStyling.css';
-import { API_KEY } from '../apidata/ApiKey';
+import { API_KEY } from '../Data/ApiKey';
+
+
+const [degress, setDegrees] = useState(null)
+const [location, setLocation] = useState("")
+const [description, setDescription] = useState("")
+const [icon, setIcon] = useState("")
+const [humidity, setHumidity] = useState(null)
+const [wind, setWind] = useState(null)
+const [country, setCountry] = useState("")
+
+const WeatherDataFetch = async () => {
+    const results = await axios.get("")
+    const weatherData = await results.weatherData
+
+    console.log(weatherData)
+}
+
+useEffect(() => {
+    WeatherDataFetch()
+})
 
 const WeatherData = () => {
   return (
